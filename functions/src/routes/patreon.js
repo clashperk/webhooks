27dad70056d1ec7eb2190cc70edd965e4503f66e
Suvajit <suvajit.me@gmail.com>
2021-09-11 21:21:56 +0530

    ESLint

--- functions/src/routes/patreon.js
+++ functions/src/routes/patreon.js
@@ -6,163 +6,9 @@ const DISCORD_WEBHOOK_URL = functions.config().webhook.url;
 const SECRET = functions.config().webhook.secret;
 const BOT_TOKEN = functions.config().bot.token;
 
-async function getDiscordUser(userId) {
-	if (!userId) return null;
-	return new Promise(resolve => {
-		https.request(`https://discord.com/api/v6/users/${userId}`, {
-			method: 'GET', headers: {
-				'Authorization': `Bot ${BOT_TOKEN}`,
-				'Content-Type': 'application/json'
-			}
-		}, res => {
-			let raw = '';
-			res.on('data', chunk => {
-				raw += chunk;
-			});
-			res.on('end', () => {
-				console.log(raw);
-				if (res.statusCode === 200) {
-					resolve(JSON.parse(raw));
-				} else { resolve(null); }
-			});
-		}).end();
-	});
-}
-
-async function webhook(embed) {
-	const payload_json = JSON.stringify({
-		username: 'ClashPerk',
-		avatar_url: 'https://i.imgur.com/bpYiIsV.png',
-		embeds: [embed]
-	});
-	https.request(DISCORD_WEBHOOK_URL, {
-		method: 'POST', headers: {
-			'Content-Type': 'application/json'
-		}
-	}, res => {
-		res.on('end', () => {
-			console.log(res.statusCode);
-		});
-	}).end(payload_json);
-}
-
-async function pledgeCreate(body) {
-	const patron_user = body.included.find(inc => inc.type === 'user');
-	const discord_id = patron_user.attributes.social_connections &&
-		patron_user.attributes.social_connections.discord &&
-		patron_user.attributes.social_connections.discord.user_id;
-
-	const { attributes } = body.data;
-	if (attributes.currently_entitled_amount_cents === 0) return;
-
-	const embed = {
-		timestamp: new Date(attributes.pledge_relationship_start),
-		description: [
-			'**Patron**',
-			`${patron_user.attributes.full_name} (${patron_user.id})`,
-			'',
-			'**Entitled Amount**',
-			`$ ${attributes.currently_entitled_amount_cents / 100}`
-		].join('\n'),
-		footer: {
-			text: 'Pledge Create'
-		},
-		color: 0xf96854
-	};
-
-	const user = await getDiscordUser(discord_id);
-
-	if (user) {
-		embed.author = {
-			name: `${user.username}#${user.discriminator}`,
-			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
-		};
-	}
-
-	return webhook(embed);
-}
-
-async function pledgeUpdate(body) {
-	const patron_user = body.included.find(inc => inc.type === 'user');
-	const discord_id = patron_user.attributes.social_connections &&
-		patron_user.attributes.social_connections.discord &&
-		patron_user.attributes.social_connections.discord.user_id;
-
-	const { attributes } = body.data;
-
-	const embed = {
-		timestamp: new Date(attributes.pledge_relationship_start),
-		description: [
-			'**Patron**',
-			`${patron_user.attributes.full_name} (${patron_user.id})`,
-			'',
-			'**Entitled Amount**',
-			`$ ${attributes.currently_entitled_amount_cents / 100}`
-		].join('\n'),
-		footer: {
-			text: 'Pledge Update'
-		},
-		color: 0x38d863
-	};
-
-	const user = await getDiscordUser(discord_id);
-
-	if (user) {
-		embed.author = {
-			name: `${user.username}#${user.discriminator}`,
-			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
-		};
-	}
-
-	return webhook(embed);
-}
-
-async function pledgeDelete(body) {
-	const patron_user = body.included.find(inc => inc.type === 'user');
-	const discord_id = patron_user.attributes.social_connections &&
-		patron_user.attributes.social_connections.discord &&
-		patron_user.attributes.social_connections.discord.user_id;
-
-	const { attributes } = body.data;
-
-	const embed = {
-		timestamp: new Date(attributes.pledge_relationship_start),
-		description: [
-			'**Patron**',
-			`${patron_user.attributes.full_name} (${patron_user.id})`,
-			'',
-			'**Lifetime Support**',
-			`$ ${attributes.lifetime_support_cents / 100}`
-		].join('\n'),
-		footer: {
-			text: 'Pledge Delete'
-		},
-		color: 0xf30c11
-	};
-
-	const user = await getDiscordUser(discord_id);
-
-	if (user) {
-		embed.author = {
-			name: `${user.username}#${user.discriminator}`,
-			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
-		};
-	}
-
-	return webhook(embed);
-}
-
-
-router.get('/patreon', (req, res) => {
-	if (req.query.token !== SECRET) {
-		return res.status(403).json({ method: 'GET', status: 403 });
-	}
-	return res.json({ method: 'GET', status: 200 });
-});
+router.get('/patreon', (req, res) => res.json({ method: 'GET', status: 200 }));
 
 router.post('/patreon', async (req, res) => {
-	console.log(req.body);
-	const action = { action: req.headers['x-patreon-event'] };
 	if (req.query.token === SECRET) {
 		switch (req.headers['x-patreon-event']) {
 			case 'members:pledge:create':
@@ -180,5 +26,135 @@ router.post('/patreon', async (req, res) => {
 	}
 	return res.status(403).json({ status: 403 });
 });
+
+async function getDiscordUser(userId) {
+	if (!userId) return null;
+	return new Promise(resolve => {
+		https.request(`https://discord.com/api/v6/users/${userId}`, {
+			method: 'GET', headers: {
+				'Authorization': `Bot ${BOT_TOKEN}`,
+				'Content-Type': 'application/json'
+			}
+		}, res => {
+			let raw = '';
+			res.on('data', chunk => {
+				raw += chunk;
+			});
+			res.on('end', () => {
+				if (res.statusCode === 200) {
+					resolve(JSON.parse(raw));
+				} else {
+					resolve(null);
+				}
+			});
+		}).end();
+	});
+}
+
+async function executeWebhook(embed) {
+	https.request(DISCORD_WEBHOOK_URL, {
+		method: 'POST', headers: {
+			'Content-Type': 'application/json'
+		}
+	}, res => {
+		res.on('end', () => {
+			console.log(res.statusCode);
+		});
+	}).end(JSON.stringify({ embeds: [embed] }));
+}
+
+async function pledgeCreate(body) {
+	const patron = body.included?.find(inc => inc.type === 'user');
+	const discordId = patron?.attributes?.social_connections?.discord.user_id;
+
+	const { attributes } = body.data;
+	if (attributes.currently_entitled_amount_cents === 0) return;
+
+	const embed = {
+		timestamp: new Date(attributes.pledge_relationship_start),
+		description: [
+			'**Patron**',
+			`${patron.attributes.full_name} (${patron.id})`,
+			'',
+			'**Entitled Amount**',
+			`$ ${attributes.currently_entitled_amount_cents / 100}`
+		].join('\n'),
+		footer: { text: 'Pledge Create' },
+		color: 0xf96854
+	};
+
+	const user = await getDiscordUser(discordId);
+
+	if (user) {
+		embed.author = {
+			name: `${user.username}#${user.discriminator} (${user.id})`,
+			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
+		};
+	}
+
+	return executeWebhook(embed);
+}
+
+async function pledgeUpdate(body) {
+	const patron = body.included?.find(inc => inc.type === 'user');
+	const discordId = patron?.attributes?.social_connections?.discord.user_id;
+
+	const { attributes } = body.data;
+
+	const embed = {
+		timestamp: new Date(attributes.pledge_relationship_start),
+		description: [
+			'**Patron**',
+			`${patron.attributes.full_name} (${patron.id})`,
+			'',
+			'**Entitled Amount**',
+			`$ ${attributes.currently_entitled_amount_cents / 100}`
+		].join('\n'),
+		footer: { text: 'Pledge Update' },
+		color: 0x38d863
+	};
+
+	const user = await getDiscordUser(discordId);
+
+	if (user) {
+		embed.author = {
+			name: `${user.username}#${user.discriminator} (${user.id})`,
+			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
+		};
+	}
+
+	return executeWebhook(embed);
+}
+
+async function pledgeDelete(body) {
+	const patron = body.included?.find(inc => inc.type === 'user');
+	const discordId = patron?.attributes?.social_connections?.discord.user_id;
+
+	const { attributes } = body.data;
+
+	const embed = {
+		timestamp: new Date(attributes.pledge_relationship_start),
+		description: [
+			'**Patron**',
+			`${patron.attributes.full_name} (${patron.id})`,
+			'',
+			'**Lifetime Support**',
+			`$ ${attributes.lifetime_support_cents / 100}`
+		].join('\n'),
+		footer: { text: 'Pledge Delete' },
+		color: 0xf30c11
+	};
+
+	const user = await getDiscordUser(discordId);
+
+	if (user) {
+		embed.author = {
+			name: `${user.username}#${user.discriminator} (${user.id})`,
+			icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
+		};
+	}
+
+	return executeWebhook(embed);
+}
 
 module.exports = router;
--- functions/src/routes/voting.js
+++ functions/src/routes/voting.js
@@ -2,30 +2,14 @@ const functions = require('firebase-functions');
 const router = require('express').Router();
 const https = require('https');
 
-function getRandom(max, min) {
-	return Math.round(Math.random() * (max - min)) + Math.round(min);
-}
-
 const DISCORD_WEBHOOK_URL = functions.config().webhook.url;
 const SECRET = functions.config().webhook.secret;
 const BOT_TOKEN = functions.config().bot.token;
 
-router.get('/voting', (req, res) => {
-	if (req.headers.authorization !== SECRET) {
-		return res.status(403).json({ method: 'GET', status: 403 });
-	}
-	return res.json({ method: 'GET', status: 200 });
-});
+router.get('/voting', (req, res) => res.json({ method: 'GET', status: 200 }));
 
 router.post('/voting', async (req, res) => {
 	if (req.headers.authorization === SECRET && req.body.type === 'upvote') {
-		const last_voted = Date.now();
-		let xp = getRandom(150, 100);
-		if (req.body.isWeekend) xp *= 2;
-		if (data) {
-			if (!req.body.isWeekend && (Date.now() - new Date(Number(data.last_voted))) <= 8.64e+7) xp *= 1.2;
-		}
-		const earnedXP = { earnedXP: Math.round(xp) };
 		try {
 			const user = await new Promise(resolve => {
 				https.request(`https://discord.com/api/v6/users/${req.body.user}`, {
@@ -47,24 +31,8 @@ router.post('/voting', async (req, res) => {
 				}).end();
 			});
 
-			if (!user) return;
-			const payload_json = JSON.stringify({
-				username: 'ClashPerk',
-				avatar_url: 'https://i.imgur.com/bpYiIsV.png',
-				embeds: [
-					{
-						author: {
-							name: `${user.username}#${user.discriminator}`,
-							icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`
-						},
-						footer: {
-							text: `Received ${earnedXP.earnedXP} XP`
-						},
-						color: 0x38d863,
-						timestamp: new Date()
-					}
-				]
-			});
+			if (!user) return functions.logger.warn('User not found!');
+
 			https.request(DISCORD_WEBHOOK_URL, {
 				method: 'POST', headers: {
 					'Content-Type': 'application/json'
@@ -73,8 +41,25 @@ router.post('/voting', async (req, res) => {
 				res.on('end', () => {
 					console.log(res.statusCode);
 				});
-			}).end(payload_json);
-		} catch { }
+			}).end(JSON.stringify({
+				username: 'ClashPerk',
+				avatar_url: 'https://i.imgur.com/bpYiIsV.png',
+				embeds: [
+					{
+						author: {
+							name: `${user.username}#${user.discriminator}`,
+							icon_url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
+						},
+						footer: {
+							text: 'Just Voted!'
+						},
+						timestamp: new Date()
+					}
+				]
+			}));
+		} catch {
+			return res.status(500).json({ method: 'POST', status: 500 });
+		}
 		return res.json({ method: 'POST', status: 200 });
 	}
 
